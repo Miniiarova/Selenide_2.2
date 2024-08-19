27ed6c76b0af43038982b01a51e00d621f607473
MarinaM <marina.miniiarova@gmail.com>
2024-08-19 21:13:15 +0700

    Исправлены ошибки в тестах

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,8 +1,10 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
@@ -10,26 +12,37 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.CollectionCondition.*;
 
 public class RegistrationTest {
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+
+        Configuration.browserCapabilities = options;
+    }
+
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
-        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(date);
+
+        String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement dateElement = $("[data-test-id=date] input");
+        dateElement.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        dateElement.setValue(date);
+
         $("[data-test-id=name] input").setValue("Иван Петров");
         $("[data-test-id=phone] input").setValue("+71111111111");
         $("[data-test-id=agreement]").click();
         $("form button.button").click();
 
-
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
-
-
-
+        $("[data-test-id=notification]")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно забронирована на " + date));
     }
 }
